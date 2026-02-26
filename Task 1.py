470ee6f499c23d89ca735e65538ac9781ae9c333
ankittsarkar <ankitt.sarkar@gmail.com>
2026-02-27 00:19:23 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,16 @@
+# Task 1: Perform Basic Mathematical Operations
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Calculating basic mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying the results
+print(f"Addition = {addition}")
+print(f"Subtraction = {subtraction}")
+print(f"Multiplication = {multiplication}")
+print(f"Division = {division}")
